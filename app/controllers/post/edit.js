413b6cff7f6f0dd54ad5d17e21294c71e134c5f8
dcuddhy <dcuddhy@gmail.com>
2015-02-03 16:57:56 -0700

    create and update are functioning.  still have to delete

---
+++ app/controllers/post/edit.js
@@ -0,0 +1,21 @@
+import Ember from "ember";
+
+export default Ember.ObjectController.extend({
+  reversedPosts: function () {
+
+  },
+
+  actions: {
+    editPosts: function() {
+      var title = this.get('title');
+
+      var body = this.get('body');
+
+        this.set('title', title);
+        this.set('body', body);
+        this.get('model').save().then(function(){
+      this.transitionToRoute('posts');
+      }.bind(this));
+    }
+  }
+});
